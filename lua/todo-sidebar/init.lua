0a6c6f9720745dc6ca94aa5dc4e5d1eb1cdac1b6
ethanamaher <ethanamaher@gmail.com>
2025-05-16 20:28:34 -0500

    initial highlight grouping logic

--- lua/todo-sidebar/init.lua
+++ lua/todo-sidebar/init.lua
@@ -4,25 +4,28 @@ local Sidebar = require("todo-sidebar.sidebar")
 local TodoSidebar = {}
 TodoSidebar.__index = TodoSidebar
 
+---@return table metatable for TodoSidebar
 function TodoSidebar:new()
-    -- initialize with default config
-    local config = Config.get_default_config()
-    return setmetatable({
-        config = config,
-        sidebar = Sidebar:new(config)
-    }, self)
+	-- initialize with default config
+	local config = Config.get_default_config()
+	return setmetatable({
+		config = config,
+		sidebar = Sidebar:new(config),
+	}, self)
 end
 
 local inst = TodoSidebar:new()
 
+---@param self table
+---@param user_opts table config
 function TodoSidebar.setup(self, user_opts)
-    if self ~= inst then
-        user_opts = self
-        self = inst
-    end
+	if self ~= inst then
+		user_opts = self
+		self = inst
+	end
 
-    -- works for setting new keymap
-    self.sidebar.sidebar_config = Config.add_config(user_opts, self.config)
+	-- works for setting new keymap
+	self.sidebar.sidebar_config = Config.add_config(user_opts, self.config)
 end
 
 return inst
